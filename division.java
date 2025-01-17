69c733dde5ff7e378380219bd4e92d515aa4604a
Krishna gopal singh <krishnagopalsingh587@gmail.com>
2025-01-17 13:46:07 +0530

    sixth commit

---
+++ division.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class division {
+    public static void main(String[] args) {
+         Scanner sc= new Scanner(System.in);
+        int n1= sc.nextInt();
+        int n2= sc.nextInt();
+        int n3= sc.nextInt();
+
+        int val = n1/n2/n3;
+        System.out.println(val);
+    }
+}
